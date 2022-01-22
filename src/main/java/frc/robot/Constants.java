48fa00b8ffed44e1c7a324dc784d2317ce5d71c0
Varun Warrier <varunswarrier@gmail.com>
2022-01-22 12:11:30 -0500

    shooter pid

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -36,9 +36,17 @@ public final class Constants {
 
     }
 
-    public final static class ShooterConstants {
+    public final static class   ShooterConstants {
         public final static int LEFT_SHOOTER_MOTOR = 18;
         public final static int RIGHT_SHOOTER_MOTOR = 1;
+        public final static double REVERSE_SHOOT_SPEED = -1.0;
+
+        public final static double MAX_RPM = 6500.0;
+        public static final double ENCODER_RESOLUTION = 2048.0;
+        public static final double PULLEY_RATIO = 48.0 / 36.0;
+        public static final double ENCODER_TIME_CONVERSION = 600.0; // minutes per 100 ms
+
+
     }
     public final static class NeckConstants {
         public static final int NECK_MOTOR_FRONT = 17; //placeholder
--- src/main/java/frc/robot/commands/shooter/SpinShooterCommand.java
+++ src/main/java/frc/robot/commands/shooter/SpinShooterCommand.java
@@ -4,15 +4,32 @@
 
 package frc.robot.commands.shooter;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.Constants.ShooterConstants;
 import frc.robot.subsystems.ShooterSubsystem;
 
-public class SpinShooterCommand extends CommandBase {
+public class SpinShooterCommand extends PIDCommand {
   private final ShooterSubsystem shooterSubsystem;
   private final double power;
   public SpinShooterCommand(ShooterSubsystem shooterSubsystem, double power) {
+    super(
+      new PIDController(0.001, 0.0, 0.0),
+      shooterSubsystem::getFlywheelRPM,
+      ShooterConstants.MAX_RPM,
+      output -> {
+        // Use the output here
+        System.out.println("Shooter PID Output: " + output);
+        System.out.println("Flywheel RPM: " + shooterSubsystem.getFlywheelRPM());
+        shooterSubsystem.shootFlywheel(0.85 + output);
+      }
+    );
     this.shooterSubsystem = shooterSubsystem;
     this.power = power;
+
+    getController().setTolerance(10.0);
+    addRequirements(shooterSubsystem);
   }
 
   // Called when the command is initially scheduled.
@@ -23,13 +40,13 @@ public class SpinShooterCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    shooterSubsystem.spinShooter(power);
+    shooterSubsystem.shootFlywheel(power);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    shooterSubsystem.spinShooter(0);
+    shooterSubsystem.shootFlywheel(0);
   }
 
   // Returns true when the command should end.
